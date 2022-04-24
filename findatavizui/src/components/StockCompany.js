59c50676a00fe72c3173d8db4804d226253c6fd6
unknown <deepenrp@yahoo.com>
2022-04-24 00:48:27 -0400

    Created Node JS Backend

--- findatavizui/src/components/StockCompany.js
+++ findatavizui/src/components/StockCompany.js
@@ -1,12 +1,24 @@
+// import React {useEffect, useState} from 'react';
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 function StockCompany() {
+
+    // useEffect( () => {
+    //     fetchItems();
+    // }, []);
+
+    // const [items, setItems] = useState([]);
+
+    // const fetchItems = async () => {
+    //     const mockData = await fetch('/stock');
+    //     const items = await mockData.json();
+    //     setItems(items);
+    //     console.log(items);
+    // };
+
     return(
-        <section>
-            <div class="container-fluid">
-                <h1>STOCK ANALYSIS</h1>
-            </div>
-        </section>
+        <h1>STOCK ANALYSIS</h1>
     );
 }
 
